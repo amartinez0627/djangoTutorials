c40cf3fe6003f4e4e7bf8bb8f6116e1ae721d066
amartinez <antoniomartinez12347@gmail.com>
2018-11-16 15:04:22 -0600

    added labels to almost all sliders

--- homeworksite/homeworksite/urls.py
+++ homeworksite/homeworksite/urls.py
@@ -22,3 +22,4 @@ urlpatterns = [
     path('gamefinderapp/', include('gamefinderapp.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
